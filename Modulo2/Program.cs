ed14835b4dfbf2ff98dc44b530213184e2d0b93e
Kauan <kauanhindlmayer07@gmail.com>
2022-11-04 10:51:43 -0300

    Introdução Dapper

--- Modulo2/Program.cs
+++ Modulo2/Program.cs
@@ -5,7 +5,7 @@ using Modulo2.Models;
 
 namespace Modulo2
 {
-	class Program
+  class Program
   {
     static void Main(string[] args)
     {
@@ -13,14 +13,67 @@ namespace Modulo2
 
       using (var connection = new SqlConnection(connectionString))
       {
-        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-        foreach (var category in categories)
-        {
-          Console.WriteLine($"{category.Id} - {category.Title}");
-        }
+        UpdateCategory(connection);
+        ListCategories(connection);
+        // CreateCategory(connection);
       }
+    }
 
-      Console.WriteLine("Hello, World!");
+    static void ListCategories(SqlConnection connection)
+    {
+      var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+      foreach (var item in categories)
+      {
+        Console.WriteLine($"{item.Id} - {item.Title}");
+      }
+    }
+
+    static void CreateCategory(SqlConnection connection)
+    {
+      var category = new Category();
+      category.Id = Guid.NewGuid();
+      category.Title = "Amazon AWS";
+      category.Url = "amazon";
+      category.Description = "Categoria destinada a serviços AWS";
+      category.Order = 8;
+      category.Summary = "AWS Cloud";
+      category.Featured = false;
+
+      var insertSql = @"
+      INSERT INTO 
+        [Category] 
+      VALUES(
+        @Id, 
+        @Title, 
+        @Url, 
+        @Summary, 
+        @Order, 
+        @Description, 
+        @Featured
+      )";
+
+      var rows = connection.Execute(insertSql, new {
+        category.Id,
+        category.Title,
+        category.Url,
+        category.Summary,
+        category.Order,
+        category.Description,
+        category.Featured
+      });
+
+      Console.WriteLine($"{rows} linhas inseridas");
+    }
+
+    static void UpdateCategory(SqlConnection connection) 
+    {
+      var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+      var rows = connection.Execute(updateQuery, new {
+        id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+        title = "Frontend 2022"
+      });
+
+      Console.WriteLine($"{rows} registros atualizados");
     }
   }
 }
