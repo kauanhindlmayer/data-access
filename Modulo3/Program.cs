d42236759684f45b30189ba8130fba4a22fc2503
Kauan <kauanhindlmayer07@gmail.com>
2022-11-04 15:16:14 -0300

    Select In

--- Modulo3/Program.cs
+++ Modulo3/Program.cs
@@ -310,5 +310,22 @@ namespace Modulo3
         }
       }
     }
+
+    static void SelectIn(SqlConnection connection)
+    {
+      var query = @"SELECT * FROM [Career] WHERE [Id] IN @Id";
+
+      var items = connection.Query<Career>(query, new {
+        Id = new[] {
+          "4327ac7e-963b-4893-9f31-9a3b28a4e72b",
+          "e6730d1c-6870-4df3-ae68-438624e04c72"
+        }
+      });
+
+      foreach (var item in items)
+      {
+        Console.WriteLine(item.Title);
+      }
+    }
   }
 }
