373fe989c815e0a30bb9a79e00a241693b16ed48
Kauan <kauanhindlmayer07@gmail.com>
2022-11-04 10:18:26 -0300

    Alterando os nomes das pastas

---
+++ Modulo2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Modulo2.Models;
+
+namespace Modulo2
+{
+	class Program
+  {
+    static void Main(string[] args)
+    {
+      const string connectionString = "Server=localhost,1433;Database=baltaDB;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+
+      using (var connection = new SqlConnection(connectionString))
+      {
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+        foreach (var category in categories)
+        {
+          Console.WriteLine($"{category.Id} - {category.Title}");
+        }
+      }
+
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
