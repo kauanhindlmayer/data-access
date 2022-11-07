77b9b41bf209464c73552c11f34ab04252f51f32
Kauan <kauanhindlmayer07@gmail.com>
2022-11-07 15:28:10 -0300

    Atualizando e deletando usuário

--- Desafio/Screens/UserScreens/DeleteUserScreen.cs
+++ Desafio/Screens/UserScreens/DeleteUserScreen.cs
@@ -8,7 +8,30 @@ namespace Desafio.Screens.UserScreens
   {
     public static void Load()
     {
+      Console.WriteLine("Deletar um usuário");
+      Console.WriteLine("-------------");
+
+      Console.WriteLine("Qual o Id do usuário deseja excluir? ");
+      var id = Console.ReadLine();
       
+      Delete(int.Parse(id));
+      Console.ReadKey();
+      MenuUserScreen.Load();
+    }
+
+    public static void Delete(int id)
+    {
+      try
+      {
+        var repository = new Repository<User>();
+        repository.Delete(id);
+        Console.WriteLine("Usuário deletado com sucesso!");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Não foi possível deletar o usuário!");
+        Console.WriteLine(ex.Message);
+      }
     }
   }
 }
--- Desafio/Screens/UserScreens/UpdateUserScreen.cs
+++ Desafio/Screens/UserScreens/UpdateUserScreen.cs
@@ -8,7 +8,54 @@ namespace Desafio.Screens.UserScreens
   {
     public static void Load()
     {
-      
+      Console.WriteLine("Atualizando um Usuário");
+      Console.WriteLine("----------------------");
+
+      Console.WriteLine("Id: ");
+      var id = Console.ReadLine();
+
+      Console.WriteLine("Name: ");
+      var name = Console.ReadLine();
+
+      Console.WriteLine("E-mail: ");
+      var email = Console.ReadLine();
+
+      Console.WriteLine("Senha: ");
+      var password = Console.ReadLine();
+
+      Console.WriteLine("Bio: ");
+      var bio = Console.ReadLine();
+
+      Console.WriteLine("Slug: ");
+      var slug = Console.ReadLine();
+
+      Update(new User
+      {
+        Id = int.Parse(id),
+        Name = name,
+        Email = email,
+        PasswordHash = password,
+        Bio = bio,
+        Image = "image",
+        Slug = slug
+      });
+      Console.ReadKey();
+      MenuUserScreen.Load();
+    }
+
+    public static void Update(User user)
+    {
+      try
+      {
+        var repository = new Repository<User>();
+        repository.Update(user);
+        Console.WriteLine("Usuário atualizado com sucesso!");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Não foi possível atualizar o usuário!");
+        Console.WriteLine(ex.Message);
+      }
     }
   }
 }
