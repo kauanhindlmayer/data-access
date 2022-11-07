319438e8d4329d36da9c54dd09f5bc6dd1006294
Kauan <kauanhindlmayer07@gmail.com>
2022-11-07 09:53:28 -0300

    Repositório genérico

--- Modulo4-Blog/Program.cs
+++ Modulo4-Blog/Program.cs
@@ -15,25 +15,35 @@ namespace Blog
       connection.Open();
       ReadUsers(connection);
       ReadRoles(connection);
+      ReadTags(connection);
       connection.Close();
     }
 
     public static void ReadUsers(SqlConnection connection)
     {
-      var repository = new UserRepository(connection);
-      var users = repository.Get();
+      var repository = new Repository<User>(connection);
+      var items = repository.Get();
 
-      foreach (var user in users)
-        Console.WriteLine(user.Name);
+      foreach (var item in items)
+        Console.WriteLine(item.Name);
     }
 
     public static void ReadRoles(SqlConnection connection)
     {
-      var repository = new RoleRepository(connection);
-      var roles = repository.Get();
+      var repository = new Repository<Role>(connection);
+      var items = repository.Get();
 
-      foreach (var role in roles)
-        Console.WriteLine(role.Name);
+      foreach (var item in items)
+        Console.WriteLine(item.Name);
+    }
+
+    public static void ReadTags(SqlConnection connection)
+    {
+      var repository = new Repository<Tag>(connection);
+      var items = repository.Get();
+
+      foreach (var item in items)
+        Console.WriteLine(item.Name);
     }
   }
 }
